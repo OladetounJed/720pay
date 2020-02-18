74d6c01840ebf93f35fb4401cb9e1503ab703592
OladetounJed <oladetountemitayo@gmail.com>
2020-02-18 11:46:05 -0800

    Invalid Login Details Validation

--- assest/js/script.js
+++ assest/js/script.js
@@ -3,6 +3,7 @@
  let signIn = document.querySelector(".sign-in-btn");
  let signInForm = document.querySelector(".sign-in-form")
  let emailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+ let incorrectPassword = document.querySelector(".incorrect-password")
 
 
  email.addEventListener('keyup', function (event) {
@@ -18,9 +19,10 @@ console.log(email.value)
   signIn.addEventListener('click', function (event) {
     if (email.value === "jed@frontend.geng" && password.value === "test"){
         window.location.href = "dashboard.html";
+        
     }
-
-
-
-    
+    else {
+        incorrectPassword.innerHTML = "Incorrect Email Address or Password!";
+        
+    }
   });
